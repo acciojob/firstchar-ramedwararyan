9e296395c7ec2ebf8d166d8ca9c6c169ae5cc89e
ramedwararyan <143419537+ramedwararyan@users.noreply.github.com>
2025-07-27 16:14:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,4 +12,4 @@ function firstChar(str) {
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
 //const text = prompt("Enter text:");
-alert(firstChar(text));
+alert(firstChar(str));
